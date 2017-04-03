567888de726f1d6f41578229c5d1c6fb289ff3ba
liam stabeno and megan warnock <megan@>
2017-04-03 10:03:36 -0700

    pass save and all test

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
 public class Person {
   private String name;
   private String email;
+  private int id;
 
   public Person (String name, String email) {
     this.name = name;
     this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object otherPerson) {
+    if (!(otherPerson instanceof  Person)) {
+      return false;
+    } else {
+      Person newPerson = (Person) otherPerson;
+      return this.getName().equals(newPerson.getName()) &&
+      this.getEmail().equals(newPerson.getEmail());
+    }
   }
 
   public String getName() {
@@ -15,15 +31,23 @@ public class Person {
     return email;
   }
 
-  @Override
-  public boolean equals(Object otherPerson) {
-    if (!(otherPerson instanceof  Person)) {
-      return false;
-    } else {
-      Person newPerson = (Person) otherPerson;
-      return this.getName().equals(newPerson.getName()) &&
-             this.getEmail().equals(newPerson.getEmail());
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO persons (name, email) VALUES (:name, :email)";
+      con.createQuery(sql)
+      .addParameter("name", this.name)
+      .addParameter("email", this.email)
+      .executeUpdate();
     }
   }
+
+  public static List<Person> all() {
+    String sql = "SELECT * FROM persons";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Person.class);
+    }
+  }
+
+
 
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,8 +1,12 @@
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class PersonTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void person_instantiatesCorrectly_true() {
@@ -10,5 +14,20 @@ public class PersonTest {
     assertEquals(true, testPerson instanceof Person);
   }
 
+  @Test
+  public void save_insertOjectIntoDatabase_Person() {
+    Person testPerson = new Person ("Henry", "henry@henry.com");
+    testPerson.save();
+    assertTrue(Person.all().get(0).equals(testPerson));
+  }
 
+  @Test
+ public void all_returnsAllInstancesOfPerson_true() {
+   Person firstPerson = new Person("Henry", "henry@henry.com");
+   firstPerson.save();
+   Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+   secondPerson.save();
+   assertEquals(true, Person.all().get(0).equals(firstPerson));
+   assertEquals(true, Person.all().get(1).equals(secondPerson));
+ }
 }
